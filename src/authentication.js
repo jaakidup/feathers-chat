331d9fac927abc8738bbf40e359b7d4bcc66658b
jaakidup <jaakidup@gmail.com>
2020-06-08 16:02:24 +0200

    initial commit

---
+++ src/authentication.js
@@ -0,0 +1,30 @@
+const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
+const { LocalStrategy } = require('@feathersjs/authentication-local');
+const { expressOauth, OAuthStrategy } = require('@feathersjs/authentication-oauth');
+
+class GitHubStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    return {
+      ...baseData,
+      // you can also set the display name to profile.name
+      name: profile.login,
+      // the github profile image
+      avatar: profile.avatar_url,
+      // user email if available
+      email: profile.email
+    };
+  }
+}
+
+module.exports = app => {
+  const authentication = new AuthenticationService(app);
+
+  authentication.register('jwt', new JWTStrategy());
+  authentication.register('local', new LocalStrategy());
+  authentication.register('github', new GitHubStrategy());
+
+  app.use('/authentication', authentication);
+  app.configure(expressOauth());
+};
